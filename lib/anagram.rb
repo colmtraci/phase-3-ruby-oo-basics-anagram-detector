3ea64bab71b4b9e3820b8436ccf46b37026276d4
Colm Traci <ctraci@me.com>
2023-01-31 15:00:37 -0800

    commit tree df1cc09363452d7729dbf412702adf11a898deb9

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
+# require "pry"
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        chars_array =  @word.chars.sort
+        # iterate through arr
+        arr.filter {|word| word.chars.sort == chars_array}
+        # check if array of characters is equal to array of character of instance variable @word
+        #   #chars then #sort on each
+    end
+
+end
+#Anagram.new "hello"
+#binding.pry
+0
